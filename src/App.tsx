57de2d59a2136de14b01f7283ed83e738a265e83
caroldelira <caroldelira@hotmail.com>
2024-07-16 21:40:52 -0300

    inicio projeto

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,20 @@
-import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
+import logoUido from './imagens/logoUIDO.png';
+
+
+export function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header id='Header'>
+        <img src={logoUido} alt="" />
+        <div>
+          <a href="#">Início</a>
+          <a href="#">Início</a>
+          <a href="#">Início</a>
+          <a href="#">Início</a>
+        </div>
       </header>
     </div>
   );
 }
-
-export default App;
